8bfc573bd9125095e44e36df587d190ab84ec4e1
margarita <m.saifullina@indoleads.com>
2021-04-28 12:45:23 +0800

    Fix typos

--- lesson2_1step8.py
+++ lesson2_1step8.py
@@ -1,12 +1,14 @@
 class NonPositiveError(Exception):
     pass
 
+
 class PositiveList(list):
     def append(self, x):
         if x > 0:
             super().append(x)
         else:
             raise NonPositiveError(x, " is not positive")
+
 
 x = PositiveList()
 print(x)
@@ -16,4 +18,3 @@ x.append(0)
 print(x)
 x.append(-1)
 print(0)
-    
