79342ac4eb20d1e68899afd7c2f37ec1901c9875
margarita <m.saifullina@indoleads.com>
2021-04-28 12:36:33 +0800

    Add a new task

---
+++ lesson2_1step8.py
@@ -0,0 +1,19 @@
+class NonPositiveError(Exception):
+    pass
+
+class PositiveList(list):
+    def append(self, x):
+        if x > 0:
+            super().append(x)
+        else:
+            raise NonPositiveError(x, " is not positive")
+
+x = PositiveList()
+print(x)
+x.append(3)
+print(x)
+x.append(0)
+print(x)
+x.append(-1)
+print(0)
+    
