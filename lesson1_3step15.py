6f3bf186cc16603d2de601e801e05430a71caf7e
margarita <m.saifullina@indoleads.com>
2021-04-28 12:24:26 +0800

    Fix typos

--- lesson1_3step15.py
+++ lesson1_3step15.py
@@ -1,6 +1,7 @@
 print("Enter two integer: first second\n")
 n, k = map(int, input().split())
 
+
 def c_n_k(n, k):
     if k == 0:
         return 1
@@ -8,6 +9,7 @@ def c_n_k(n, k):
         return 0
     else:
         return c_n_k(n-1, k) + c_n_k(n-1, k-1)
- 
+
+
 c = c_n_k(n, k)
 print(c)
